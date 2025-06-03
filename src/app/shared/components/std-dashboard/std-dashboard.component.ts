db4bb0fb98b24991217cc0fd9bf053ebc8c8e66d
akash kale <kale64136@gmail.com>
2025-06-03 22:53:27 +0530

    first commit

--- src/app/shared/components/std-dashboard/std-dashboard.component.ts
+++ src/app/shared/components/std-dashboard/std-dashboard.component.ts
@@ -15,58 +15,58 @@ export class StdDashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  sdtArray : Array<istd>= [
-    {
-      fname : "aksh",
-      lname : "kale",
-      email : "aksh@gmail.com",
-      contact : 9467343783,
-      stdId : "212121213"
-    },
-    {
-      fname : "laksh",
-      lname : "upadyay",
-      email : "laksh@gmail.com",
-      contact : 9987654236,
-      stdId : "21212121"
-    },
-    {
-      fname : "avinash",
-      lname : "kale",
-      email : "avinash@gmail.com",
-      contact : 956346733,
-      stdId : "212121214"
-    },
-    {
-      fname : "soham",
-      lname : ",amoj",
-      email : "shil@gmail.com",
-      contact : 956346777733,
-      stdId : "2121212140"
-    }
-  ]
+  // sdtArray : Array<istd>= [
+  //   {
+  //     fname : "aksh",
+  //     lname : "kale",
+  //     email : "aksh@gmail.com",
+  //     contact : 9467343783,
+  //     stdId : "212121213"
+  //   },
+  //   {
+  //     fname : "laksh",
+  //     lname : "upadyay",
+  //     email : "laksh@gmail.com",
+  //     contact : 9987654236,
+  //     stdId : "21212121"
+  //   },
+  //   {
+  //     fname : "avinash",
+  //     lname : "kale",
+  //     email : "avinash@gmail.com",
+  //     contact : 956346733,
+  //     stdId : "212121214"
+  //   },
+  //   {
+  //     fname : "soham",
+  //     lname : ",amoj",
+  //     email : "shil@gmail.com",
+  //     contact : 956346777733,
+  //     stdId : "2121212140"
+  //   }
+  // ]
 
-  newFormdata(data: istd){
-    const index = this.sdtArray.findIndex(s => s.stdId === data.stdId);
-    if (index > -1) {
-      this.sdtArray[index] = data;
-    } else {
-      this.sdtArray.push(data);
-    }
+  // newFormdata(data: istd){
+  //   const index = this.sdtArray.findIndex(s => s.stdId === data.stdId);
+  //   if (index > -1) {
+  //     this.sdtArray[index] = data;
+  //   } else {
+  //     this.sdtArray.push(data);
+  //   }
 
-  }
+  // }
 
 
   editstudent(std : istd){
     this.editedstd =std
   }
 
-
-  deletdStd(stdId : string){
-    this.sdtArray = this.sdtArray.filter(std => std.stdId !== stdId);
-    console.log(stdId);
+ 
+  // deletdStd(stdId : string){
+  //   this.sdtArray = this.sdtArray.filter(std => std.stdId !== stdId);
+  //   console.log(stdId);
     
-  }
+  // }
 
   generateUUID(): string {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
