bd7427393af6ece3a0f2b141c6d2adc5b914d424
akash kale <kale64136@gmail.com>
2025-06-05 23:25:51 +0530

    first commit

---
+++ src/app/shared/services/snackbar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+ constructor( private snackBar : MatSnackBar){}
+
+    OpenSnackBar(msg : string){
+    let Matconfig : MatSnackBarConfig ={
+        horizontalPosition :'center',
+        verticalPosition : "bottom",
+        duration : 3000
+    }
+        this.snackBar.open(msg, "close", Matconfig)
+
+    }
+}
